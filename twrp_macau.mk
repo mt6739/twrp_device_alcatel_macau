372ef92de3c793a7bc26551abb65fdeda4886d0b
mertsan2007 <110729187+mertsan2007@users.noreply.github.com>
2025-09-02 09:22:44 +0300

    Initial commit

---
+++ twrp_macau.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+DEVICE_PATH := device/alcatel/macau
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults_512.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Charger
+PRODUCT_PACKAGES += charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_macau
+PRODUCT_DEVICE := macau
+PRODUCT_MODEL := Alcatel 1 2021
+PRODUCT_BRAND := alcatel
+PRODUCT_MANUFACTURER := TCL
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sys_mssi_32_ago_ww-user 11 RP1A.200720.011 RB4E release-keys"
+
+BUILD_FINGERPRINT := TCL/macau/macau:11/RP1A.200720.011/RB4E:user/release-keys
